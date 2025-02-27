452b2023471771a6e234ec1e0ccca4ecbddd7d7b
VenkatW360 <venkat@wellness360.co>
2025-02-27 16:57:35 +0530

    Initial commit

---
+++ src/main/java/com/sheetsclone/GoogleSheetsCloneApplication.java
@@ -0,0 +1,13 @@
+package com.sheetsclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSheetsCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleSheetsCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sheetsclone/GoogleSheetsCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheetsclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleSheetsCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
